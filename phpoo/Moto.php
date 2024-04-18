8a251ff8937e427217c01101ed16cef08a28a731
Gabriel <gabrielmmve13@gmail.com>
2024-04-18 19:53:15 -0300

    criando classe moto

---
+++ phpoo/Moto.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "Veiculo.php";
+
+class Moto extends Veiculo {
+    private $velocidade;
+    private $ligado = false;
+    private $parado;
+
+    public function ligar() {
+        if($this->ligado == false) {
+            $this->ligado = true;
+            echo "a moto $this->modelo esta ligada <br>";
+        }
+    }
+
+    public function acelerar() {
+        if ($this->ligado == false) {
+            echo "a moto $this->modelo não está ligada";
+        } else {
+            $this->velocidade += 5;
+            echo "a moto $this->modelo está acelerando para $this->velocidade km/h <br>";
+        }
+    }
+
+    public function frear() {
+        if ($this->velocidade > 0) {
+            $this-> velocidade -= 5;
+            echo "a moto $this->modelo reduziu para: $this->velocidade km/h <br>";
+        }
+    }
+
+    public function parar() {
+        if ($this->velocidade > 0) {
+            echo "velocidade alta demais para parar <br>";
+        } else {
+            $this->parado = true;
+            echo "a moto $this->modelo parou";
+        }
+    }
+
+    public function desligado() {
+        if ($this->parado == true) {
+            echo "a moto $this->modelo esta desligado <br>";
+            $this->ligado = false;
+        } else {
+            echo "a moto não esta parada";
+        }
+    }
+
+}
+
